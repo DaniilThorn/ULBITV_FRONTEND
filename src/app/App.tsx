f5018a13d2cd46a77b3b2afaf1f5e4d1ab81ad0f
Daniil Pylenok <work.pylenok@gmail.com>
2023-08-05 17:33:57 +0300

    react refresh plugin, babel, i18n babel extract plugin, Eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
-
 import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { Navbar } from "widgets/Navbar";
 import { AppRouter } from "./providers/router";
 import { Sidebar } from "widgets/Sidebar";
-
-import {Suspense} from 'react'
+import { Suspense } from "react";
 import { useTranslation } from "react-i18next";
 
 export enum Theme {
@@ -15,16 +13,16 @@ export enum Theme {
 }
 
 export const App = () => {
-  const { theme } = useTheme();
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <Suspense fallback="">
-      <Navbar />
-      <div className="content-page">
-        <Sidebar/>      
-        <AppRouter/>
-      </div>
-      </Suspense>
-    </div>
-  );
+    const { theme } = useTheme();
+    return (
+        <div className={classNames("app", {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import cls from "./Navbar.module.scss";
@@ -7,17 +8,18 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  return (
-    <div className={classNames(cls.navbar, {}, [className])}>
+    const {t} = useTranslation();
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
      
-      <div className={cls.links}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-          Главная
-        </AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-          О сайте
-        </AppLink>
-      </div>
-    </div>
-  );
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                    {t("Главная страница")}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+                    {t("О сайте")}
+                </AppLink>
+            </div>
+        </div>
+    );
 };
