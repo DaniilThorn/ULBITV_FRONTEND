17dfd230eed72a826ce9822edc52547d0ca7c2a3
Daniil Pylenok <work.pylenok@gmail.com>
2024-02-17 19:25:52 +0300

    Sidebar & Article list

--- src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticleDetailsPage.tsx
@@ -1,51 +1,73 @@
-import {classNames} from 'shared/lib/classNames/classNames'
+import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './ArticleDetailsPage.module.scss'
 import { useTranslation } from 'react-i18next'
-import { memo } from 'react'
-import { useParams } from 'react-router-dom'
+import { memo, useCallback } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { Text } from 'shared/ui/Text/Text'
 import { ArticleDetails } from '../../../../entities/Article'
 import { CommentList } from '../../../../entities/Comment'
-import { DynamicModuleLoader, ReducersList }
-  from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
-import { articleDetailsCommentsReducer, getArticleComments }
-  from '../../model/slices/ArticleDetailsCommentsSlice'
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import {
+  articleDetailsCommentsReducer,
+  getArticleComments,
+} from '../../model/slices/ArticleDetailsCommentsSlice'
 import { useDispatch, useSelector } from 'react-redux'
-import { getArticleCommentsError, getArticleCommentsIsLoading }
-  from '../../model/selectors/comments'
-import { fetchCommentsByArticleId } from 'pages/ArticlesDetailsPage/model/services/fetchCommentsByArticleId'
+import {
+  getArticleCommentsError,
+  getArticleCommentsIsLoading,
+} from '../../model/selectors/comments'
+import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId'
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect'
+import { AddCommentForm } from 'features/AddNewComment'
+import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle'
+import { Button } from 'shared/ui/Button/Button'
+import { RoutePath } from 'shared/config/routeConfig/routeConfig'
 
 interface ArticlesDetailsPageProps {
-     className?: string
+  className?: string
 }
 
-const reducers:ReducersList = {
-  articleDetailsComments: articleDetailsCommentsReducer}
+const reducers: ReducersList = {
+  articleDetailsComments: articleDetailsCommentsReducer,
+}
 
-const ArticleDetailsPage = ({className, }:ArticlesDetailsPageProps) => {
-  const {t} = useTranslation("article");
-  const { id } = useParams<{id:string}>()
+const ArticleDetailsPage = ({ className }: ArticlesDetailsPageProps) => {
+  const { t } = useTranslation('article')
+  const navigate = useNavigate()
+  const { id } = useParams<{ id: string }>()
   const dispatch = useDispatch()
   const comments = useSelector(getArticleComments.selectAll)
   const commentsIsLoading = useSelector(getArticleCommentsIsLoading)
   const commentsError = useSelector(getArticleCommentsError)
 
-  useInitialEffect(()=> dispatch(fetchCommentsByArticleId(id)))
+  const onBackToList = useCallback(() => {
+    navigate(RoutePath.articles)
+  }, [])
 
-  if(!id){
+  const onSendComment = useCallback((text: string) => {
+    dispatch(addCommentForArticle(text))
+  }, [])
+
+  useInitialEffect(() => dispatch(fetchCommentsByArticleId(id)))
+
+  if (!id) {
     return (
       <div className={classNames(cls.articledetailspage, {}, [className])}>
-        {t("Статья не найдена")}
+        {t('Статья не найдена')}
       </div>
     )
   }
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
       <div className={classNames(cls.articledetailspage, {}, [className])}>
-        <ArticleDetails id={id}/>
-        <Text className={cls.commentTitle} title={t("Комментарии")}/>
-        <CommentList  comments={comments} isLoading={commentsIsLoading}/>
+        <Button onClick={onBackToList}>{t('Назад к списку')}</Button>
+        <ArticleDetails id={id} />
+        <Text className={cls.commentTitle} title={t('Комментарии')} />
+        <AddCommentForm onSendComment={onSendComment} />
+        <CommentList comments={comments} isLoading={commentsIsLoading} />
       </div>
     </DynamicModuleLoader>
   )
--- src/pages/profilePage/ui/ProfilePageHeader/ProfilePageHeader.tsx
+++ src/pages/profilePage/ui/ProfilePageHeader/ProfilePageHeader.tsx
@@ -1,64 +1,68 @@
-import {classNames} from 'shared/lib/classNames/classNames'
+import { classNames } from 'shared/lib/classNames/classNames'
 import { Button, ButtonTheme } from 'shared/ui/Button/Button'
 import cls from './ProfilePageHeader.module.scss'
-import { Text } from "shared/ui/Text/Text"
+import { Text } from 'shared/ui/Text/Text'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
-import { getProfileReadonly, profileActions, updateProfileData } from '../../../../entities/Profile'
+import {
+  getProfileData,
+  getProfileReadonly,
+  profileActions,
+  updateProfileData,
+} from '../../../../entities/Profile'
 import { useCallback } from 'react'
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch'
+import { getUserAuthData } from '../../../../entities/User'
 
 interface ProfilePageHeaderProps {
-     className?: string
-    
+  className?: string
 }
 
-export const ProfilePageHeader = ({className, }:ProfilePageHeaderProps) => {
-    
+export const ProfilePageHeader = ({ className }: ProfilePageHeaderProps) => {
   const { t } = useTranslation('profile')
 
   const readonly = useSelector(getProfileReadonly)
+  const authData = useSelector(getUserAuthData)
+  const profileData = useSelector(getProfileData)
+  const canEdit = authData?.id === profileData?.id
   const dispatch = useAppDispatch()
 
-  const onEdit = useCallback(()=>{
+  const onEdit = useCallback(() => {
     dispatch(profileActions.setReadonly(false))
-  },[dispatch])
+  }, [dispatch])
 
-  const onCancelEdit = useCallback(()=>{
+  const onCancelEdit = useCallback(() => {
     dispatch(profileActions.cancelEdit())
-  },[dispatch])
+  }, [dispatch])
 
-  const onSave = useCallback(()=>{
+  const onSave = useCallback(() => {
     dispatch(updateProfileData())
-  },[dispatch])
+  }, [dispatch])
   return (
     <div className={classNames(cls.profilePageHeader, {}, [className])}>
-      <Text title={t('Профиль')}/>
-      {readonly
-        ? (
-          <Button
-            className={cls.editBtn}
-            theme={ButtonTheme.OUTLINE}
-            onClick={onEdit}
-          >{t('Редактировать')}
-          </Button>
-        )
-        :(
-          <div  className={cls.editBlock}>
-            <Button
-              className={cls.editBtn}
-              theme={ButtonTheme.OUTLINE}
-              onClick={onSave}
-            >{t('Сохранить')}</Button>
-            <Button
-              className={cls.editBtn}
-              theme={ButtonTheme.OUTLINE_RED}
-              onClick={onCancelEdit}
-            >{t('Отменить')}</Button>
-          </div>
-        )
-      }
-     
+      <Text title={t('Профиль')} />
+      {canEdit && (
+        <div className={cls.btnsWrapper}>
+          {readonly ? (
+            <Button theme={ButtonTheme.OUTLINE} onClick={onEdit}>
+              {t('Редактировать')}
+            </Button>
+          ) : (
+            <div>
+              <Button
+                className={cls.editBtn}
+                theme={ButtonTheme.OUTLINE}
+                onClick={onSave}
+              >
+                {t('Сохранить')}
+              </Button>
+              <Button theme={ButtonTheme.OUTLINE_RED} onClick={onCancelEdit}>
+                {t('Отменить')}
+              </Button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
